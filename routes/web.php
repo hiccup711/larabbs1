0b37f40ecc34256b12481f1d07e01908957c8e33
Ricky <hiccup711@gmail.com>
2020-10-22 05:31:04 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 // 用户相关
 Route::resource('users', 'UsersController', [ 'only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
