ad095d953f96592d59acfaee0051be831aa88165
Ricky <hiccup711@gmail.com>
2020-10-23 03:38:48 +0000

    添加导航active样式判断

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,8 @@ function default_avatar()
 {
     return config('app.url') . '/uploads/images/avatars/avatar.png';
 }
+
+function category_nav_active($category_id)
+{
+    return active_class(if_route('categories.show') && if_route_param('category', $category_id));
+}
