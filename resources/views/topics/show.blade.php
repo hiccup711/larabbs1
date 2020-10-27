22efa268034aa032c8c8db1702dae407a6bc6295
vagrant <vagrant@homestead>
2020-10-27 08:04:57 +0000

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -37,6 +37,7 @@
                 <div class="topic-body mt-4 mb-4">
                     {!! $topic->content !!}
                 </div>
+                @can('update', $topic)
                 <div class="operate">
                     <hr>
                     <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
@@ -48,9 +49,18 @@
                         {{ method_field('DELETE') }}
                         <button class="btn btn-outline-secondary btn-sm" type="submit">
                             <i class="far fa-trash-alt"></i> 删除
-                        </button> 
+                        </button>
                     </form>
                 </div>
+                @endcan
+            </div>
+        </div>
+
+{{--        用户回复列表--}}
+        <div class="card topic-reply mt-4">
+            <div class="card-body">
+                @include('topics._reply_box', ['topic' => $topic])
+                @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
             </div>
         </div>
     </div>
