32c109546ab0ac90a509486a9142bbc9740c7cbf
Ricky <hiccup711@gmail.com>
2020-11-05 11:21:05 +0000

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'Api\VerificationCodesController@store')
-        ->name('verificationCodes.store');
+//    登录相关
+    Route::middleware('throttle'. config('api.rate_limits.access')->group(function (){
 
-    Route::post('users', 'Api\UsersController@store')->name('users.store');
+        // 短信验证码
+        Route::post('verificationCodes', 'Api\VerificationCodesController@store')
+            ->name('verificationCodes.store');
+
+        Route::post('users', 'Api\UsersController@store')->name('users.store');
+    }));
+
+//    访问相关
+    Route::middleware('throttle'. config('api.rate_limits.access'))->group(function (){
+
+    });
 });
